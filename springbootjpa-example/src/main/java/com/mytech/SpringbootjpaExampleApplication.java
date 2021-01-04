dd41464a663d5b93d1ffd43f6cc829a0667db006
Brajesh Pandey <pandey.brajesh9718@gmail.com>
2021-01-04 14:24:38 +0530

    Add and Commit

---
+++ springbootjpa-example/src/main/java/com/mytech/SpringbootjpaExampleApplication.java
@@ -0,0 +1,13 @@
+package com.mytech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootjpaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootjpaExampleApplication.class, args);
+	}
+
+}
---
+++ springbootjpa-example/src/test/java/com/mytech/SpringbootjpaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootjpaExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
